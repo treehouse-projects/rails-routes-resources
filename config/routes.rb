7c1b1ab1e9c46a16d5e3515b71685c8378b4bd9b
Jay McGavren <jay@mcgavren.com>
2016-09-02 15:56:42 -0700

    Add view for new pages.

--- config/routes.rb
+++ config/routes.rb
@@ -1,6 +1,6 @@
 Rails.application.routes.draw do
   resources :posts
   get '/pages', to: 'pages#index'
-  get '/pages/new', to: 'pages#new'
+  get '/pages/new', to: 'pages#new', as: 'new_page'
   get '/pages/:id', to: 'pages#show', as: 'page'
 end
