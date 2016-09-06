890ace14b93a9db126be43ea467acecfe266ce35
Jay McGavren <jay@mcgavren.com>
2016-09-06 13:38:45 -0700

    Add PagesController#create.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -10,4 +10,11 @@ class PagesController < ApplicationController
   def new
     @page = Page.new
   end
+
+  def create
+    page_params = params.require(:page).permit(:title, :body, :slug)
+    @page = Page.new(page_params)
+    @page.save
+    redirect_to @page
+  end
 end
