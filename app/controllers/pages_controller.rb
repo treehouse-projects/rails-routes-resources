5c53d8d57bedba0b2eaf5ea89a0006877b47e59f
Jay McGavren <jay@mcgavren.com>
2016-09-07 16:33:42 -0700

    Add PagesController#update.

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -12,7 +12,6 @@ class PagesController < ApplicationController
   end
 
   def create
-    page_params = params.require(:page).permit(:title, :body, :slug)
     @page = Page.new(page_params)
     @page.save
     redirect_to @page
@@ -21,4 +20,16 @@ class PagesController < ApplicationController
   def edit
     @page = Page.find(params[:id])
   end
+
+  def update
+    @page = Page.find(params[:id])
+    @page.update(page_params)
+    redirect_to @page
+  end
+
+  private
+
+    def page_params
+      params.require(:page).permit(:title, :body, :slug)
+    end
 end
